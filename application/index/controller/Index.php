ba88610c1f9a6762ae25251a6b028e471ad1168a
xiaoming <xiaoming@qq.com>
2020-05-13 15:41:10 +0800

    显示

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,12 @@ namespace app\index\controller;
 
 class Index
 {
+
+    /**
+     * 轮播图数据显示
+     *
+     * @return void
+     */
     public function imageShow()
     {
         $data = model("common/Show")->bgShow(0, $limit = 5);
