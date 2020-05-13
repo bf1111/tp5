a95dc84213d4e165fa7cb5fe2c4ec6b2c25c20cd
xiaoming <xiaoming@qq.com>
2020-05-13 14:38:38 +0800

    大图展示

---
+++ application/common/model/Show.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+class Show extends Model
+{
+    /**
+     * 大图展示
+     *
+     * @param [type] $position  展示的位置
+     * @param [type] $limit  显示的数量
+     * @return void
+     */
+    public function bgShow($position, $limit = 5)
+    {
+        $data = [
+            'status' => 1,
+            'position' => $position
+        ];
+        $order = [
+            'id' => 'desc'
+        ];
+        return $this->where($data)->limit($limit)->order($order)->select();
+    }
+}
